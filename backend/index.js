51f09a6acc99e118ffd69cb98a9d18c7993bd75f
Adora <adoraonyile@gmail.com>
2024-12-07 21:47:20 +0100

    updated link

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://scn-v1.vercel.app/"],
+    origin: ["https://scn-v1.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
